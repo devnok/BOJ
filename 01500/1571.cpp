d388fe56090271998195225c5e1e22e55b24696f
cyberinforoom <48426972+devnok@users.noreply.github.com>
2021-12-27 12:14:52 +0000

    REPL.IT 초기설정 및 폴더명 변경

---
+++ 01500/1571.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <bitset>
+#include <functional>
+#include <chrono>
+#include <random>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+using vint = vector<int>;
+using vll = vector<ll>;
+using vld = vector<ld>;
+using vpii = vector<pii>;
+using vpil = vector<pil>;
+using vpli = vector<pli>;
+using vpll = vector<pll>;
+
+#define x first
+#define y second
+#define all(v) (v).begin(), (v).end()
+#define cpr(v) sort(all(v)); (v).erase(unique(all(v)), (v).end())
+#define idx(v, x) int(lower_bound(all(v), (x)) - (v).begin())
+#define ints(args...) int args; readln(args);
+#define lls(args...) ll args; readln(args);
+#define strs(args...) string args; readln(args);
+
+template<typename... Args>
+void readln(Args&... args) { ((cin >> args), ...); }
+template<typename... Args>
+void writeln(Args... args) { ((cout << args << " "), ...); cout << '\n'; }
+
+int main(void){
+  cin.tie(0)->sync_with_stdio(0);
+  
+}
