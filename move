17390d8c7ce48eac180992a1667c0e3afd5aa96f
devnok <ha819ha.dev@gmail.com>
2022-03-04 15:19:21 +0000

    vscode-settings

--- move
+++ move
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir -p "AC/$1"
 mv "$1"/*.cpp AC/"$1"/
 rmdir $1
--- others/hi.cpp
+++ others/hi.cpp
@@ -30,35 +30,5 @@ void writeln(Args... args) { ((cout << args << " "), ...); cout << '\n'; }
 
 int main(void){
   cin.tie(0)->sync_with_stdio(0);
-  lls(n);
-  vll a(n);
-  for(ll& i:a) cin>>i;
-  sort(all(a));
-  ll d=a[1]-a[0];
-  if(d<1){
-    cout<<"NO";
-    return 0;
-  }
-  multiset<ll> m1(all(a));
-  ll k;
-  for(ll i=a[1];;i+=d){
-    auto it = m1.find(i);
-    if(it == m1.end()) break;
-    m1.erase(it);
-    k=i;
-  }
-  auto m2(m1);
-  m2.insert(k);
-  auto chk = [](multiset<ll> m){
-    if(m.size()<2) return 0;
-    ll s = *m.begin();
-    ll d = *(++m.begin()) - s;
-    if(d<1) return 0;
-    for(auto it=m.begin();it!=m.end();++it){
-      if(s!=*it) return 0;
-      s+=d;
-    }
-    return 1;
-  };
-  cout<<(chk(m1)||chk(m2) ? "YES" : "NO");
+  
 }
